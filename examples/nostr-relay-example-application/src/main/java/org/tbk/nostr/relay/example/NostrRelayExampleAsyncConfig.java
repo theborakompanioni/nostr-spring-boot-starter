0debc29f92a6f43dca7d8a95ffce8c4a09904a0e
theborakompanioni <theborakompanioni+github@gmail.com>
2024-02-09 12:58:08 +0100

    example(relay): graceful async task executor shutdown

--- examples/nostr-relay-example-application/src/main/java/org/tbk/nostr/relay/example/NostrRelayExampleAsyncConfig.java
+++ examples/nostr-relay-example-application/src/main/java/org/tbk/nostr/relay/example/NostrRelayExampleAsyncConfig.java
@@ -1,7 +1,11 @@
 package org.tbk.nostr.relay.example;
 
+import jakarta.persistence.EntityManagerFactory;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,8 +14,9 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.time.Duration;
 import java.util.concurrent.Executor;
 
+@Slf4j
 @EnableAsync
-@Configuration(proxyBeanMethods = false)
+@Configuration
 @RequiredArgsConstructor
 class NostrRelayExampleAsyncConfig implements AsyncConfigurer {
 
@@ -20,6 +25,11 @@ class NostrRelayExampleAsyncConfig implements AsyncConfigurer {
 
     @Override
     public Executor getAsyncExecutor() {
+        return asyncThreadPoolTaskExecutor();
+    }
+
+    @Bean
+    ThreadPoolTaskExecutor asyncThreadPoolTaskExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
         threadPoolTaskExecutor.setThreadNamePrefix("tbk-async-");
         threadPoolTaskExecutor.setCorePoolSize(1);
@@ -29,9 +39,29 @@ class NostrRelayExampleAsyncConfig implements AsyncConfigurer {
         threadPoolTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
         threadPoolTaskExecutor.setAwaitTerminationSeconds((int) Duration.ofMinutes(2).toSeconds());
 
-        // must call initialize otherwise @Async methods won't work!
-        threadPoolTaskExecutor.initialize();
-
         return threadPoolTaskExecutor;
+    }
+
+    /**
+     * Async tasks that were submitted often need a database connection.
+     * This bean should ensure that the entity manager factory is closed after the task executors shuts down.
+     */
+    @Bean
+    DisposableBean gracefulAsyncTaskExecutorShutdown(ThreadPoolTaskExecutor asyncThreadPoolTaskExecutor,
+                                                     EntityManagerFactory entityManagerFactory) {
+        return () -> {
+            log.info("Commencing graceful async task executor shutdown. Waiting for {} active async tasks to complete. {} tasks queued.",
+                    asyncThreadPoolTaskExecutor.getActiveCount(),
+                    asyncThreadPoolTaskExecutor.getQueueSize());
+
+            if (!entityManagerFactory.isOpen()) {
+                log.warn("Shutting down async task executor, but entity manager factory already closed. " +
+                         "Already queued tasks that need database access will fail. ");
+            }
+
+            asyncThreadPoolTaskExecutor.shutdown();
+
+            log.info("Graceful async task executor shutdown complete.");
+        };
     }
 }
