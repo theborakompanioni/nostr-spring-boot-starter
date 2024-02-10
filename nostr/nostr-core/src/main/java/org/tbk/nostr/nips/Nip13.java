025740e34518fc8bec057f15683c16e9dfa287b5
theborakompanioni <theborakompanioni+github@gmail.com>
2024-02-10 18:46:06 +0100

    fix(nip13): verify createdAt within bounds

--- nostr/nostr-core/src/main/java/org/tbk/nostr/nips/Nip13.java
+++ nostr/nostr-core/src/main/java/org/tbk/nostr/nips/Nip13.java
@@ -6,7 +6,6 @@ import org.tbk.nostr.proto.TagValue;
 import org.tbk.nostr.util.MoreEvents;
 import org.tbk.nostr.util.MoreTags;
 
-import java.time.Instant;
 import java.util.BitSet;
 import java.util.stream.IntStream;
 
@@ -16,6 +15,10 @@ import java.util.stream.IntStream;
 public final class Nip13 {
     private Nip13() {
         throw new UnsupportedOperationException();
+    }
+
+    public static TagValue nonce(long nonce, long targetDifficulty) {
+        return MoreTags.named("nonce", Long.toString(nonce), Long.toString(targetDifficulty));
     }
 
     public static long calculateDifficulty(EventOrBuilder event) {
@@ -28,29 +31,36 @@ public final class Nip13 {
 
     public static Event.Builder mineEvent(Event.Builder prototype, int targetDifficulty) {
         int prototypeTagCount = prototype.getTagsCount();
-        long nonce = 0;
+        long nonce = 0; // could also start at Long.MIN_VALUE, but starting at 0 produces smaller events (in bytes)
 
-        TagValue.Builder nonceTagBuilder = MoreTags.named("nonce", Long.toString(nonce), Long.toString(targetDifficulty)).toBuilder();
-        Event.Builder builder = Event.newBuilder(prototype.buildPartial()).addTags(nonceTagBuilder);
+        TagValue.Builder nonceTagBuilder = nonce(nonce, targetDifficulty).toBuilder();
+        Event.Builder eventBuilder = Event.newBuilder(prototype.buildPartial()).addTags(nonceTagBuilder);
 
         long currentEpoch = System.currentTimeMillis() / 1_000;
         long currentDifficulty = 0;
         while (currentDifficulty < targetDifficulty) {
             long runEpoch = System.currentTimeMillis() / 1_000;
-            if (runEpoch > currentEpoch) {
+            if (runEpoch != currentEpoch) {
                 currentEpoch = runEpoch;
-                builder.setCreatedAt(Instant.now().toEpochMilli());
-                nonce = 0;
+
+                if (eventBuilder.getCreatedAt() != currentEpoch) {
+                    eventBuilder.setCreatedAt(currentEpoch);
+                    nonce = 0; // nonce can be reset when "created_at" changed
+                }
             }
+            // it is unreasonable to think that this code runs on hardware that can cycle through half of all longs in 1s
+            /* else if (Long.MAX_VALUE == nonce) {
+                nonce = Long.MIN_VALUE;
+            }*/
 
-            // update "nonce" tag
-            builder.setTags(prototypeTagCount, nonceTagBuilder.setValues(0, Long.toString(nonce)));
+            // update "nonce" tag with current nonce value
+            eventBuilder.setTags(prototypeTagCount, nonceTagBuilder.setValues(0, Long.toString(nonce)));
 
-            currentDifficulty = countLeadingZeroes(MoreEvents.eventId(builder));
+            currentDifficulty = countLeadingZeroes(MoreEvents.eventId(eventBuilder));
             nonce++;
         }
 
-        return MoreEvents.withEventId(builder);
+        return MoreEvents.withEventId(eventBuilder);
     }
 
     private static long countLeadingZeroes(byte[] bytes) {
--- nostr/nostr-core/src/test/java/org/tbk/nostr/nips/Nip13Test.java
+++ nostr/nostr-core/src/test/java/org/tbk/nostr/nips/Nip13Test.java
@@ -3,13 +3,18 @@ package org.tbk.nostr.nips;
 import com.google.common.base.Stopwatch;
 import fr.acinq.bitcoin.XonlyPublicKey;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 import org.tbk.nostr.identity.SimpleSigner;
 import org.tbk.nostr.proto.Event;
+import org.tbk.nostr.proto.EventRequest;
+import org.tbk.nostr.proto.Request;
 import org.tbk.nostr.proto.TagValue;
+import org.tbk.nostr.proto.json.JsonWriter;
 import org.tbk.nostr.util.MoreTags;
 
+import java.time.Instant;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -35,6 +40,8 @@ class Nip13Test {
         Event event = Nip13.mineEvent(Nip1.createTextNote(testPubkey, content), targetDifficulty).build();
 
         assertThat(Nip13.calculateDifficulty(event), is(greaterThanOrEqualTo((long) targetDifficulty)));
+        assertThat(event.getCreatedAt(), is(both(greaterThanOrEqualTo(Instant.now().minusSeconds(1).getEpochSecond()))
+                .and(lessThanOrEqualTo(Instant.now().plusSeconds(1).getEpochSecond()))));
 
         TagValue nonceTag = MoreTags.filterTagsByName("nonce", event).getFirst();
         assertThat("it took zero tries", nonceTag.getValues(0), is("0"));
@@ -50,10 +57,12 @@ class Nip13Test {
         Event event = Nip13.mineEvent(Nip1.createTextNote(testPubkey, content), targetDifficulty).build();
 
         assertThat(Nip13.calculateDifficulty(event), is(greaterThanOrEqualTo((long) targetDifficulty)));
+        assertThat(event.getCreatedAt(), is(both(greaterThanOrEqualTo(Instant.now().minusSeconds(1).getEpochSecond()))
+                .and(lessThanOrEqualTo(Instant.now().plusSeconds(1).getEpochSecond()))));
 
         String bitString = toBitString(event.getId().toByteArray());
         assertThat(bitString, hasLength(256));
-        assertThat(bitString, startsWith("0".repeat(targetDifficulty)));
+        assertThat("it has enough zero bits", bitString, startsWith("0".repeat(targetDifficulty)));
 
         TagValue nonceTag = MoreTags.filterTagsByName("nonce", event).getFirst();
         assertThat("it committed to target difficulty", nonceTag.getValues(1), is(Long.toString(targetDifficulty)));
@@ -70,10 +79,12 @@ class Nip13Test {
         Event event = Nip13.mineEvent(Nip1.createTextNote(testPubkey, content), targetDifficulty).build();
 
         assertThat(Nip13.calculateDifficulty(event), is(greaterThanOrEqualTo((long) targetDifficulty)));
+        assertThat(event.getCreatedAt(), is(both(greaterThanOrEqualTo(Instant.now().minusSeconds(1).getEpochSecond()))
+                .and(lessThanOrEqualTo(Instant.now().plusSeconds(1).getEpochSecond()))));
 
         String bitString = toBitString(event.getId().toByteArray());
         assertThat(bitString, hasLength(256));
-        assertThat(bitString, startsWith("0".repeat(targetDifficulty)));
+        assertThat("it has enough zero bits", bitString, startsWith("0".repeat(targetDifficulty)));
 
         TagValue nonceTag = MoreTags.filterTagsByName("nonce", event).getFirst();
         assertThat("it committed to target difficulty", nonceTag.getValues(1), is(Long.toString(targetDifficulty)));
@@ -90,14 +101,53 @@ class Nip13Test {
         Event event = Nip13.mineEvent(Nip1.createTextNote(testPubkey, content), targetDifficulty).build();
 
         assertThat(Nip13.calculateDifficulty(event), is(greaterThanOrEqualTo((long) targetDifficulty)));
+        assertThat(event.getCreatedAt(), is(both(greaterThanOrEqualTo(Instant.now().minusSeconds(1).getEpochSecond()))
+                .and(lessThanOrEqualTo(Instant.now().plusSeconds(1).getEpochSecond()))));
 
         String bitString = toBitString(event.getId().toByteArray());
         assertThat(bitString, hasLength(256));
-        assertThat(bitString, startsWith("0".repeat(targetDifficulty)));
+        assertThat("it has enough zero bits", bitString, startsWith("0".repeat(targetDifficulty)));
 
         TagValue nonceTag = MoreTags.filterTagsByName("nonce", event).getFirst();
         assertThat("it committed to target difficulty", nonceTag.getValues(1), is(Long.toString(targetDifficulty)));
 
         log.info("difficulty({}): Took {} and {} tries in the last epoch to find {}", targetDifficulty, stopwatch.stop(), nonceTag.getValues(0), bitString);
+    }
+
+    /**
+     * Some benchmarks on a "reasonable consumer cpu" (in 2023):
+     * - difficulty(19): 594.0 ms, 1.049 s, 1.869 s
+     * - difficulty(20): 804.6 ms, 1.596 s, 5.459 s
+     * - difficulty(21): Took 4.697 s, 5.517 s, 10.11 s
+     * - difficulty(22): Took 6.650 s, 7.027 s, 13.35 s
+     * - difficulty(23): Took 11.77 s, 25.58 s, 46.87 s
+     * - difficulty(24): Took 1.830 min, 2.485 min, 2.841 min
+     * - difficulty(25): Took 36.15 s, 43.45 s, 2.962 min
+     */
+    @RepeatedTest(3)
+    @Disabled("Enable on demand - should not execute on every test suite run")
+    void mineEventManually(RepetitionInfo info) {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        int targetDifficulty = 21;
+
+        String content = "GM-%d-%d".formatted(targetDifficulty, info.getCurrentRepetition());
+        Event event = Nip13.mineEvent(Nip1.createTextNote(testPubkey, content), targetDifficulty).build();
+
+        assertThat(Nip13.calculateDifficulty(event), is(greaterThanOrEqualTo((long) targetDifficulty)));
+        assertThat(event.getCreatedAt(), is(both(greaterThanOrEqualTo(Instant.now().minusSeconds(1).getEpochSecond()))
+                .and(lessThanOrEqualTo(Instant.now().plusSeconds(1).getEpochSecond()))));
+
+        String bitString = toBitString(event.getId().toByteArray());
+        assertThat(bitString, hasLength(256));
+        assertThat("it has enough zero bits", bitString, startsWith("0".repeat(targetDifficulty)));
+
+        TagValue nonceTag = MoreTags.filterTagsByName("nonce", event).getFirst();
+        assertThat("it committed to target difficulty", nonceTag.getValues(1), is(Long.toString(targetDifficulty)));
+
+        log.info("difficulty({}): Took {} and {} tries in the last epoch to find {}", targetDifficulty, stopwatch.stop(), nonceTag.getValues(0), bitString);
+
+        log.info("{}", JsonWriter.toJson(Request.newBuilder()
+                .setEvent(EventRequest.newBuilder().setEvent(event).build())
+                .build()));
     }
 }
