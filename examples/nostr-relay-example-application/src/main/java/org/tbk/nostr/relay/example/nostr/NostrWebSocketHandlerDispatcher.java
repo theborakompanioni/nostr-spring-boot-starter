10f21c017f5ec9af5736ff4d6a9da3d465253306
theborakompanioni <theborakompanioni+github@gmail.com>
2024-02-12 03:36:15 +0100

    example(relay): wrap websocket session

--- examples/nostr-relay-example-application/src/main/java/org/tbk/nostr/relay/example/nostr/NostrWebSocketHandlerDispatcher.java
+++ examples/nostr-relay-example-application/src/main/java/org/tbk/nostr/relay/example/nostr/NostrWebSocketHandlerDispatcher.java
@@ -4,10 +4,14 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.handler.WebSocketSessionDecorator;
 import org.tbk.nostr.proto.Request;
 import org.tbk.nostr.proto.json.JsonReader;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 public class NostrWebSocketHandlerDispatcher extends TextWebSocketHandler {
@@ -25,26 +29,71 @@ public class NostrWebSocketHandlerDispatcher extends TextWebSocketHandler {
 
     @Override
     protected final void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        WebSocketSessionWrapper sessionWrapper = new WebSocketSessionWrapper(session);
+
         Request request = null;
         try {
             request = JsonReader.fromJson(message.getPayload(), Request.newBuilder());
         } catch (Exception e) {
-            handler.handleJsonParseException(session, message, e);
+            handler.handleJsonParseException(sessionWrapper, message, e);
         }
 
         if (request != null) {
-            if (!executionChain.applyPreHandle(session, request)) {
+            if (!executionChain.applyPreHandle(sessionWrapper, request)) {
                 return;
             }
 
-            executionChain.applyHandle(session, request, handler);
+            executionChain.applyHandle(sessionWrapper, request, handler);
 
-            executionChain.applyPostHandle(session, request);
+            executionChain.applyPostHandle(sessionWrapper, request);
         }
+
+        sessionWrapper.tryFlushMessageBuffer();
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         handler.afterConnectionClosed(session, status);
+    }
+
+    private static class WebSocketSessionWrapper extends WebSocketSessionDecorator {
+
+        public WebSocketSessionWrapper(WebSocketSession session) {
+            super(session);
+        }
+
+        @Override
+        public void setTextMessageSizeLimit(int messageSizeLimit) {
+            throw new UnsupportedOperationException("'setTextMessageSizeLimit' has been disabled on purpose. Do it in method 'afterConnectionEstablished'.");
+        }
+
+        @Override
+        public void setBinaryMessageSizeLimit(int messageSizeLimit) {
+            throw new UnsupportedOperationException("'setBinaryMessageSizeLimit' has been disabled on purpose. Do it in method 'afterConnectionEstablished'.");
+        }
+
+        @Override
+        public void sendMessage(WebSocketMessage<?> message) throws IOException {
+            // TODO: delay sending message
+            // e.g. take a look at ConcurrentWebSocketSessionDecorator
+            this.getDelegate().sendMessage(message);
+        }
+
+        public void tryFlushMessageBuffer() {
+            // TODO: try flushing all messages in the buffer here
+            // e.g. take a look at ConcurrentWebSocketSessionDecorator
+        }
+
+        @Override
+        public void close() throws IOException {
+            tryFlushMessageBuffer();
+            this.getDelegate().close();
+        }
+
+        @Override
+        public void close(CloseStatus status) throws IOException {
+            tryFlushMessageBuffer();
+            this.getDelegate().close(status);
+        }
     }
 }
