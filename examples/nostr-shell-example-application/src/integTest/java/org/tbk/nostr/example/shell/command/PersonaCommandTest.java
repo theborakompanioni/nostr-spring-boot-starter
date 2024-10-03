2358719b99f323f1a00bf67d3faa7e028870a9c6
theborakompanioni <theborakompanioni+github@gmail.com>
2024-10-03 11:18:20 +0200

    test(shell): add test for persona bob

--- examples/nostr-shell-example-application/src/integTest/java/org/tbk/nostr/example/shell/command/PersonaCommandTest.java
+++ examples/nostr-shell-example-application/src/integTest/java/org/tbk/nostr/example/shell/command/PersonaCommandTest.java
@@ -21,7 +21,7 @@ class PersonaCommandTest {
     private ShellTestClient client;
 
     @Test
-    void testPersonaInteractive() {
+    void testPersonaAlice0() {
         ShellTestClient.InteractiveShellSession session = client
                 .interactive()
                 .run();
@@ -46,6 +46,36 @@ class PersonaCommandTest {
                     .containsText("\"publicKey\" : \"f319269a8757e84e9b6dad9325cb74933f64e9497c4c3a8f7757361e78edf564\"")
                     .containsText("\"nsec\" : \"nsec1064t9a0fxkd6mdfcwghz8ehxtwcvsfj6wp7nzlkykyeve536adeqjksgqj\"")
                     .containsText("\"npub\" : \"npub17vvjdx582l5yaxmd4kfjtjm5jvlkf62f03xr4rmh2umpu78d74jqxhkuj6\"")
+                    .containsText("}");
+        });
+    }
+
+    @Test
+    void testPersonaBob0() {
+        ShellTestClient.InteractiveShellSession session = client
+                .interactive()
+                .run();
+
+        await().atMost(2, TimeUnit.SECONDS).untilAsserted(() -> {
+            ShellAssertions.assertThat(session.screen())
+                    .containsText("nostr:>");
+        });
+
+        session.write(session.writeSequence()
+                .text("persona").space()
+                .text("--name").space().text("bob").space()
+                .carriageReturn()
+                .build());
+
+        await().atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
+            ShellAssertions.assertThat(session.screen())
+                    .containsText("{")
+                    .containsText("\"entropy\" : \"81b637d8fcd2c6da6359e6963113a117\"")
+                    .containsText("\"mnemonic\" : \"like random wage whale cluster honey miracle devote normal mass tribe comfort\"")
+                    .containsText("\"privateKey\" : \"db5348eb22abb023fde6015a562a56a09da6ee2458c7714cb458af8d7441e6e3\"")
+                    .containsText("\"publicKey\" : \"72603b8a1329cd9cb7e117f1d9d6ae6bb9385ec591d30a3c91ef40aa8aa4c409\"")
+                    .containsText("\"nsec\" : \"nsec1mdf536ez4wcz8l0xq9d9v2jk5zw6dm3ytrrhzn95tzhc6azpum3s9xd9wz\"")
+                    .containsText("\"npub\" : \"npub1wfsrhzsn98xeedlpzlcan44wdwunshk9j8fs50y3aaq24z4ycsysuspqy8\"")
                     .containsText("}");
         });
     }
