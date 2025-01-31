8e25f2dbe1ecb75f8359ebc0c500a3d5f9f7716d
theborakompanioni <theborakompanioni+github@gmail.com>
2025-01-31 19:23:50 +0100

    example(shell): identity-vanity command with npub suffix

--- examples/nostr-shell-example-application/src/integTest/java/org/tbk/nostr/example/shell/command/IdentityVanityCommandTest.java
+++ examples/nostr-shell-example-application/src/integTest/java/org/tbk/nostr/example/shell/command/IdentityVanityCommandTest.java
@@ -21,7 +21,7 @@ class IdentityVanityCommandTest {
     private ShellTestClient client;
 
     @Test
-    void testIdentityVanityInteractive() {
+    void testIdentityVanityPrefix() {
         ShellTestClient.InteractiveShellSession session = client
                 .interactive()
                 .run();
@@ -42,10 +42,71 @@ class IdentityVanityCommandTest {
         await().atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
             ShellAssertions.assertThat(session.screen())
                     .containsText("{")
+                    .containsText("\"npub\" : \"npub1" + npubPrefix)
+                    .containsText("\"nsec\" : \"nsec1")
                     .containsText("\"privateKey\" : \"")
                     .containsText("\"publicKey\" : \"")
+                    .containsText("}");
+        });
+    }
+
+    @Test
+    void testIdentityVanitySuffix() {
+        ShellTestClient.InteractiveShellSession session = client
+                .interactive()
+                .run();
+
+        await().atMost(2, TimeUnit.SECONDS).untilAsserted(() -> {
+            ShellAssertions.assertThat(session.screen())
+                    .containsText("nostr:>");
+        });
+
+        String npubSuffix = "z";
+
+        session.write(session.writeSequence()
+                .text("identity-vanity").space()
+                .text("--npub-suffix").space().text(npubSuffix).space()
+                .carriageReturn()
+                .build());
+
+        await().atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
+            ShellAssertions.assertThat(session.screen())
+                    .containsText("{")
+                    .containsText("\"npub\" : \"npub1").containsText(npubSuffix + "\"")
                     .containsText("\"nsec\" : \"nsec1")
-                    .containsText("\"npub\" : \"npub1" + npubPrefix)
+                    .containsText("\"privateKey\" : \"")
+                    .containsText("\"publicKey\" : \"")
+                    .containsText("}");
+        });
+    }
+
+    @Test
+    void testIdentityVanityPrefixAndSuffix() {
+        ShellTestClient.InteractiveShellSession session = client
+                .interactive()
+                .run();
+
+        await().atMost(2, TimeUnit.SECONDS).untilAsserted(() -> {
+            ShellAssertions.assertThat(session.screen())
+                    .containsText("nostr:>");
+        });
+
+        String prefixAndSuffix = "z";
+
+        session.write(session.writeSequence()
+                .text("identity-vanity").space()
+                .text("--npub-prefix").space().text(prefixAndSuffix).space()
+                .text("--npub-suffix").space().text(prefixAndSuffix).space()
+                .carriageReturn()
+                .build());
+
+        await().atMost(10 * 2, TimeUnit.SECONDS).untilAsserted(() -> {
+            ShellAssertions.assertThat(session.screen())
+                    .containsText("{")
+                    .containsText("\"npub\" : \"npub1" + prefixAndSuffix).containsText(prefixAndSuffix + "\"")
+                    .containsText("\"nsec\" : \"nsec1")
+                    .containsText("\"privateKey\" : \"")
+                    .containsText("\"publicKey\" : \"")
                     .containsText("}");
         });
     }
--- examples/nostr-shell-example-application/src/main/java/org/tbk/nostr/example/shell/command/IdentityVanityCommand.java
+++ examples/nostr-shell-example-application/src/main/java/org/tbk/nostr/example/shell/command/IdentityVanityCommand.java
@@ -1,6 +1,5 @@
 package org.tbk.nostr.example.shell.command;
 
-import com.google.common.base.Predicate;
 import com.google.common.base.Stopwatch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +15,7 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.io.IOException;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 import static java.util.Objects.requireNonNull;
@@ -29,20 +29,26 @@ class IdentityVanityCommand {
     @ShellMethod(key = "identity-vanity", value = "Generate a vanity nostr key pair")
     public String run(
             @ShellOption(value = "npub-prefix", defaultValue = "", help = "npub prefix") String npubPrefixArg,
+            @ShellOption(value = "npub-suffix", defaultValue = "", help = "npub suffix") String npubSuffixArg,
             @ShellOption(value = "parallelism", defaultValue = "0", help = "parallelism level (default: # of processors / 2)") int parallelismArg
     ) throws IOException {
         int parallelism = parallelismArg > 0 ? parallelismArg : Math.max(1, Runtime.getRuntime().availableProcessors() / 2);
         String npubPrefix = npubPrefixArg == null ? "" : npubPrefixArg;
+        String npubSuffix = npubSuffixArg == null ? "" : npubSuffixArg;
 
         if (!hasValidBech32Chars().test(npubPrefix)) {
             throw new IllegalArgumentException("npub-prefix contains invalid bech32 chars");
         }
+        if (!hasValidBech32Chars().test(npubSuffix)) {
+            throw new IllegalArgumentException("npub-suffix contains invalid bech32 chars");
+        }
 
-        log.debug("Starting identity-vanity (npub-prefix := {}, parallelism := {}", npubPrefix, parallelism);
+        log.debug("Starting identity-vanity (npub-prefix := {}, npub-suffix := {}, parallelism := {}", npubPrefix, npubSuffix, parallelism);
 
         Stopwatch stopwatch = Stopwatch.createStarted();
 
-        Identity.Account account = requireNonNull(randomAccountWithNpubHavingPrefix(npubPrefix, parallelism).block());
+        Mono<Identity.Account> accountMono = mineAccountMatching(parallelism, withNpubPrefixAndSuffix(npubPrefix, npubSuffix));
+        Identity.Account account = requireNonNull(accountMono.block());
 
         log.debug("identity-vanity with npub-prefix '{}' took {}", npubPrefix, stopwatch.stop());
 
@@ -59,17 +65,19 @@ class IdentityVanityCommand {
     private static Predicate<String> hasValidBech32Chars() {
         // from https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki
         return test -> !test.contains("1") &&
-               !test.contains("b") &&
-               !test.contains("i") &&
-               !test.contains("o");
+                       !test.contains("b") &&
+                       !test.contains("i") &&
+                       !test.contains("o");
     }
 
-    private static Predicate<Identity.Account> withNpubPrefix(String npubPrefix) {
-        return account -> Nip19.encodeNpub(account.getPublicKey()).startsWith("npub1" + npubPrefix);
+    private static Predicate<Identity.Account> withNpubPrefixAndSuffix(String prefix, String suffix) {
+        Predicate<String> prefixPredicate = npub -> npub.startsWith("npub1" + prefix);
+        Predicate<String> suffixPredicate = npub -> npub.endsWith(suffix);
+        return withNpubMatching(prefix.isEmpty() ? suffixPredicate : prefixPredicate.and(suffixPredicate));
     }
 
-    private Mono<Identity.Account> randomAccountWithNpubHavingPrefix(String npubPrefix, int parallelism) {
-        return mineAccountMatching(parallelism, withNpubPrefix(npubPrefix));
+    private static Predicate<Identity.Account> withNpubMatching(Predicate<String> matcher) {
+        return account -> matcher.test(Nip19.encodeNpub(account.getPublicKey()));
     }
 
     private Mono<Identity.Account> mineAccountMatching(int parallelism, Predicate<Identity.Account> predicate) {
@@ -96,7 +104,7 @@ class IdentityVanityCommand {
             }
 
             Identity.Account account = randomAccount();
-            if (predicate.apply(account)) {
+            if (predicate.test(account)) {
                 return account;
             }
         }
