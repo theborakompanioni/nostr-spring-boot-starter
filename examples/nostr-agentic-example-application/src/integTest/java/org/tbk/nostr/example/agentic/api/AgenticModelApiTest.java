e52256953436edfcefb10a72604b0aab0f6f9259
theborakompanioni <theborakompanioni+github@gmail.com>
2026-09-07 15:16:41 +0200

    chore(test): recreate docker containers in tests

--- examples/nostr-agentic-example-application/src/integTest/java/org/tbk/nostr/example/agentic/api/AgenticModelApiTest.java
+++ examples/nostr-agentic-example-application/src/integTest/java/org/tbk/nostr/example/agentic/api/AgenticModelApiTest.java
@@ -9,8 +9,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -29,6 +32,7 @@ class AgenticModelApiTest {
         mockMvc.perform(get("/api/v1/model/listmodels")
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.models", is(notNullValue())));
     }
 }
